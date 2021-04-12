d57437d18018db81381fb4f1635504bfa468e43c
cyrus.huang@sjfood.com <cyrus.huang@sjfood.com>
2021-04-12 14:49:58 +0800

    =jietu

--- wdio.ios.conf.js
+++ wdio.ios.conf.js
@@ -201,8 +201,9 @@ exports.config = {
     /**
      * Function to be executed before a test (in Mocha/Jasmine) starts.
      */
-    // beforeTest: function (test, context) {
-    // },
+    beforeTest: function (test, context) {
+        driver.takeScreenshot();
+    },
     /**
      * Hook that gets executed _before_ a hook within the suite starts (e.g. runs before calling
      * beforeEach in Mocha)
@@ -218,8 +219,9 @@ exports.config = {
     /**
      * Function to be executed after a test (in Mocha/Jasmine).
      */
-    // afterTest: function(test, context, { error, result, duration, passed, retries }) {
-    // },
+    afterTest: function(test, context, { error, result, duration, passed, retries }) {
+        driver.takeScreenshot();
+    },
 
 
     /**
